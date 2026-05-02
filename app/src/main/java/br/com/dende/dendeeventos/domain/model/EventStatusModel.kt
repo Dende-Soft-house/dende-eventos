bfb72b188f12606d0b5a62b217c6cc21e47bc0db
chairass <166766696+chairass@users.noreply.github.com>
2026-05-02 13:21:43 -0300

    feat: implementa data classes para feed de eventos e ingressos

---
+++ app/src/main/java/br/com/dende/dendeeventos/domain/model/EventStatusModel.kt
@@ -0,0 +1,36 @@
+package br.com.dende.dendeeventos.domain.model
+
+import java.time.LocalDateTime
+
+enum class CategoriaEvento {
+    MUSICA, GASTRONOMIA, ESPORTES, DESIGN, TECNOLOGIA, EDUCACAO, NEGOCIOS, SAUDE, ARTE, CULTURA, FESTA, WORKSHOP, PALESTRA
+}
+
+
+data class Evento(
+    val eventoId: String,
+    val nomeEvento: String,
+    val descricao: String,
+    val dataHora: LocalDateTime,
+    val duracaoMinutos: Int,
+    val gratuito: Boolean,
+    val totalParticipantes: Int,
+    val salvo: Boolean,
+    val local: Local,
+    val  categoriaEvento: CategoriaEvento
+)
+
+data class Local(
+    val id: String,
+    val nome: String,
+    val cidade: String
+)
+
+data class Ingresso(
+    val id: String,
+    val evento: Evento,
+    val tipo: String,
+    val nomeUsuario: String,
+    val statusIngresso: Boolean,
+    val qrCode: String
+)
