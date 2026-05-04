5099ce839ba882824738b79b1b837f4be658b685
Bruno Silva <76857399+BrunoSS3@users.noreply.github.com>
2026-05-03 23:19:51 -0300

    Add files via upload

---
+++ app/src/main/java/br/com/dende/dendeeventos/domain/PagamentoCompra.kt
@@ -0,0 +1,19 @@
+import java.util.UUID
+
+
+enum class StatusPagamento {
+    PENDENTE, CONFIRMADO, RECUSADO, CANCELADO
+}
+
+data class Pagamento(
+    val id: String = UUID.randomUUID().toString(),
+    val usuarioId: String,
+    val eventoId: String,
+    val cartaoId: String? = null,
+    val tipoPagamento: TipoPagamento,
+    val quantidade: Int,
+    val precoUnitario: Double,
+    val valorTotal: Double,
+    var status: StatusPagamento = StatusPagamento.PENDENTE,
+    val dataHora: 
+)
