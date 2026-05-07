bfd750d4af8ef86a0a4280d37467a93d76689a74
Nana <giovanna000gio@outlook.com>
2026-05-07 20:08:35 -0300

    Fix/Correções sugeridas pelo professor
    Corrigido -> Foram aplicadas as correções indicadas pelo professor em sala de aula (07/05)

--- app/src/main/java/br/com/dende/dendeeventos/domain/EventStatusModel.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/EventStatusModel.kt
@@ -1,15 +1,11 @@
 // Enum Class para tela de ativação e desativação de evento
 enum class EtapaDesativacaoEvento {
-    AGUARDANDO, SUCESSO, ERRO
+    AGUARDANDO_CONFIRMACAO, DESATIVADO, ERRO
 }
 
 data class EventoStatus(
     val eventoId: Int,
     var nomeEvento: String,
-    var dataInicio: LocalDateTime,
-    var dataFim: LocalDateTime,
-    var localEvento: String,
-    var imagemUrl: String,
     // Variável responsável por controlar o estado do evento (ativo/inativo)
     var ativo: Boolean = false,
     val statusOperacao: StatusOperacao = StatusOperacao.AGUARDANDO
