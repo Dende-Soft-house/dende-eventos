469eed0201c8bafa4dbd9d25a3b4b4f897547515
Carlos Henrique S S S <154102871+CarlosH-Santiago@users.noreply.github.com>
2026-05-07 21:18:21 -0300

    Refactor PerfilOrganizador to extend PerfilUsuario

--- app/src/main/java/br/com/dende/dendeeventos/domain/PerfilOrganizador.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/PerfilOrganizador.kt
@@ -1,6 +1,10 @@
 package br.com.dende.dendeeventos.domain
 
 data class PerfilOrganizador(
-    var perfilUsuario: PerfilUsuario,
+    override var nome: String,
+    override var imagemURL: String,
+    override var dataNascimento: LocalDate,
+    override var genero: Genero,
+    override var email: String,
     var DadosEmpresa: Empresa?,
-)
+) : PerfilUsuario(nome, imagemURL, dataNascimento, genero, email)
