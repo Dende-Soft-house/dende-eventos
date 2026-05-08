a613070ec4ee7ce47250af6fe8fbddc988a8770d
Leonardo Freitas <leoneryfreitas@gmail.com>
2026-05-07 21:52:48 -0300

    Adicionei e modifiquei alguns atributos nos dataclass de Evento, Local e Ingresso e criei um novo Enum para staus do evento

--- app/src/main/java/br/com/dende/dendeeventos/domain/model/EventStatusModel.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/model/EventStatusModel.kt
@@ -1,6 +1,7 @@
 package br.com.dende.dendeeventos.domain.model
 
 import java.time.LocalDateTime
+import java.time.Period
 
 enum class CategoriaEvento {
     MUSICA, GASTRONOMIA, ESPORTES, DESIGN, TECNOLOGIA, EDUCACAO, NEGOCIOS, SAUDE, ARTE, CULTURA, FESTA, WORKSHOP, PALESTRA
@@ -8,31 +9,47 @@ enum class CategoriaEvento {
 
 // Enum que representa as categorias possíveis de um evento.
 
-data class Evento(
-    val eventoId: String,
-    val nomeEvento: String,
+enum class StatusEvento {
+    ATIVO, ENCERRADO, CANCELADO
+}
+
+// Enum que representa os status possíveis de um evento.
+
+
+data class EventCard(
+    var id: Long,
+    val evento: String,
     val descricao: String,
     val dataHora: LocalDateTime,
-    val duracaoMinutos: Int,
+    val dataInicio: LocalDateTime,
+    val dataFim: LocalDateTime,
+    val duracao: Period,
     val gratuito: Boolean,
-    val totalParticipantes: Int,
-    val salvo: Boolean,
+    val totalInscritos: Int,
+    val capacidade: Int,
     val local: Local,
-    val  categoriaEvento: CategoriaEvento
-)
+    val categoriaEvento: CategoriaEvento,
+    val status: StatusEvento,
+    val urlImageBanner: String
+) {
+    fun calcularDataFim(): LocalDateTime {
+
+        return dataInicio.plus(duracao)
+    }
+}
 
 // Representa o local o evento ocorre
 data class Local(
-    val id: String,
     val nome: String,
     val cidade: String
 )
 
 data class Ingresso(
-    val id: String,
-    val evento: Evento,
-    val tipo: String,
-    val nomeUsuario: String,
-    val statusIngresso: Boolean,
-    val qrCode: String
+    var id: Long,
+    val evento: EventCard,
+    val categoriaEvento: CategoriaEvento,
+    val nomeUsuario: Usuario,
+    val statusEvento: StatusEvento,
+    val urlQrCode: String,
+    val urlImage: String
 )
