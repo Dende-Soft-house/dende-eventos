7e05459a769f4664895ea4272235837eabd2e322
Levi Falcão | Hpnos <40741874+hpnos@users.noreply.github.com>
2026-05-10 23:07:56 -0300

    Update app/src/main/java/br/com/dende/dendeeventos/domain/CancelamentoIngresso.kt
    Co-authored-by: Lucas Almeida <lucas.almeida.las@gmail.com>

--- app/src/main/java/br/com/dende/dendeeventos/domain/CancelamentoIngresso.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/CancelamentoIngresso.kt
@@ -3,7 +3,7 @@ package br.com.dende.dendeeventos.domain
 data class CancelamentoIngresso(
     val idPedido: String,
     val idIngresso: String,
-    val statusCancelamento: StatusCancelamento,
+    var statusCancelamento: StatusCancelamento.NAO_INICIADO,
     val percentualTaxaCancelamento: Int, // de 0 a 100
     val motivoCancelamento: TipoMotivoCancelamento? = null,
     val observacaoCancelamento: String? = null,
