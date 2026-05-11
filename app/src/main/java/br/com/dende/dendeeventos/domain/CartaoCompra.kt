7104cadf320546f12f8b37f3f619da82eb115319
Levi Falcão | Hpnos <40741874+hpnos@users.noreply.github.com>
2026-05-10 23:28:44 -0300

    Update app/src/main/java/br/com/dende/dendeeventos/domain/CartaoCompra.kt
    Co-authored-by: Lucas Almeida <lucas.almeida.las@gmail.com>

--- app/src/main/java/br/com/dende/dendeeventos/domain/CartaoCompra.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/CartaoCompra.kt
@@ -2,7 +2,7 @@ import java.util.UUID
 
 data class Cartao(
     val id: String = UUID.randomUUID().toString(),
-    val usuarioId: String,
+    val usuario: Usuario,
     val nomeTitular: String,
     val numero: String,
     val validade: String,
