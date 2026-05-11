639e8c9d3b21d9cbffa575c0d68adc68d502223b
Levi Falcão | Hpnos <40741874+hpnos@users.noreply.github.com>
2026-05-10 23:22:20 -0300

    Update app/src/main/java/br/com/dende/dendeeventos/domain/IngressoCompra.kt
    Co-authored-by: Lucas Almeida <lucas.almeida.las@gmail.com>

--- app/src/main/java/br/com/dende/dendeeventos/domain/IngressoCompra.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/IngressoCompra.kt
@@ -3,7 +3,7 @@ import java.util.UUID
 data class Ingresso(
     val id: String = UUID.randomUUID().toString(),
     val usuario: Usuário,
-    val eventoId: String,
+    val evento: Evento,
     val pagamentoId: String,
     val assento: String,
     var status: StatusIngresso = StatusIngresso.ATIVO
