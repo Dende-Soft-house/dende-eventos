869a69d6e923f1d5cdadcfb824d2d39614d42a0e
DeividSkii <darlandeivid64@gmail.com>
2026-05-21 16:42:26 -0300

    Ajustando data class conforme review e adicionando .idea/ ao gitignore

--- app/src/main/java/br/com/dende/dendeeventos/domain/model/EventTicketModel.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/model/EventTicketModel.kt
@@ -1,16 +1,11 @@
 package br.com.dende.dendeeventos.domain.model
 
-import java.time.LocalDateTime
-import java.time.Period
-
-enum class CategoriaEvento {
-    MUSICA, GASTRONOMIA, ESPORTES, DESIGN, TECNOLOGIA, EDUCACAO, NEGOCIOS, SAUDE, ARTE, CULTURA, FESTA, WORKSHOP, PALESTRA
-}
+import java.time.Duration
 
 // Enum que representa as categorias possíveis de um evento.
 
 enum class StatusEvento {
-    ATIVO, ENCERRADO, CANCELADO
+    ATIVO, ENCERRADO, CANCELADO, EM_EXECUCAO
 }
 
 // Enum que representa os status possíveis de um evento.
@@ -18,39 +13,28 @@ enum class StatusEvento {
 
 data class EventCard(
     var id: Long,
-    val evento: String,
-    val descricao: String,
-    val dataHora: LocalDateTime,
-    val dataHoraInicio: LocalDateTime,
-    val dataHoraFim: LocalDateTime,
-    val duracao: Period,
+    val evento: Evento,
     val gratuito: Boolean,
     val totalInscritos: Int,
     val capacidade: Int,
     val local: Local,
-    val categoriaEvento: CategoriaEvento,
+    val categoriaEvento: TipoEvento,
     val status: StatusEvento,
     val urlImageBanner: String
 ) {
-    fun calcularDuracao(): Period {
-        return Period.between(
-            dataHoraInicio.toLocalDate(),
-            dataHoraFim.toLocalDate()
+    fun calcularDuracao(): Duration {
+        return Duration.between(
+            evento.dataInicio,
+            evento.dataFim
         )
     }
 }
 
-// Representa o local o evento ocorre
-data class Local(
-    val nome: String,
-    val cidade: String
-)
-
 data class Ingresso(
     var id: Long,
-    val evento: EventCard,
+    val evento: Evento,
     val categoriaEvento: CategoriaEvento,
-    val nomeUsuario: Usuario,
+    val usuario: Usuario,
     val statusEvento: StatusEvento,
     val urlQrCode: String,
     val urlImage: String
