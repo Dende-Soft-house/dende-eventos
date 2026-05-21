5e90a4ec115e38c2bc54f07e57900c4d1877e818
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-05-21 17:09:01 -0300

    feat(login): adiciona ViewModel na tela e refatoração

--- app/src/main/java/br/com/dende/dendeeventos/ui/login/viewModel/TelaLogin.kt
+++ app/src/main/java/br/com/dende/dendeeventos/ui/login/viewModel/TelaLogin.kt
@@ -1,5 +1,6 @@
 package br.com.dende.dendeeventos.ui.login.viewModel
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,12 +14,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.draw.shadow
+
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +38,12 @@ import br.com.dende.dendeeventos.R
 private val DendeOrange = Color(0xFFF25D27)
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(
+    loginViewModel: LoginViewModel
+) {
+
+    val uiState by loginViewModel.uiState.collectAsState()
+
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -96,15 +111,25 @@ fun LoginScreen() {
                 Spacer(modifier = Modifier.height(32.dp))
 
                 VisualTextField(
+                    value = uiState.email,
+                    onValueChange = {
+                        loginViewModel.onEmailChanged(it)
+                    },
                     placeholder = "Escreva seu e-mail",
-                    icon = Icons.Outlined.Email
+                    icon = Icons.Outlined.Email,
+                    isError = !uiState.isEmailValid
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
                 VisualTextField(
+                    value = uiState.senha,
+                    onValueChange = {
+                        loginViewModel.onPasswordChanged(it)
+                    },
                     placeholder = "Digite a sua senha",
-                    icon = Icons.Outlined.Lock
+                    icon = Icons.Outlined.Lock,
+                    isError = !uiState.isPasswordValid
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
@@ -136,26 +161,46 @@ fun LoginScreen() {
 
                 Spacer(modifier = Modifier.height(24.dp))
 
-                Box(
+                Button(
+                    onClick = {
+                        loginViewModel.realizarLogin()
+                    },
+                    enabled = !uiState.isLoading,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(56.dp)
-                        .background(
-                            brush = Brush.linearGradient(
-                                colors = listOf(
-                                    Color(0xFF3E3F47),
-                                    Color(0xFF242730)
+                        .height(56.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
+                    ),
+                    contentPadding = PaddingValues(0.dp)
+                ) {
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        Color(0xFF3E3F47),
+                                        Color(0xFF242730)
+                                    )
                                 )
                             ),
-                            shape = RoundedCornerShape(12.dp)
-                        ),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = "LOGIN",
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    )
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if(uiState.isLoading) {
+                            CircularProgressIndicator(
+                                color = Color.White
+                            )
+                        } else {
+                            Text(
+                                text = "LOGIN",
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
                 }
 
                 Spacer(modifier = Modifier.weight(1f))
@@ -175,7 +220,10 @@ fun LoginScreen() {
 @Composable
 fun VisualTextField(
     placeholder: String,
-    icon: ImageVector
+    icon: ImageVector,
+    value: String,
+    onValueChange: (String) -> Unit,
+    isError: Boolean = false
 ) {
     Row(
         modifier = Modifier
@@ -183,7 +231,7 @@ fun VisualTextField(
             .height(56.dp)
             .border(
                 width = 1.dp,
-                color = Color.Gray,
+                color = if (isError) Color.Red else Color.Gray,
                 shape = RoundedCornerShape(12.dp)
             )
             .padding(horizontal = 16.dp),
@@ -192,21 +240,39 @@ fun VisualTextField(
         Icon(
             imageVector = icon,
             contentDescription = null,
-            tint = Color.DarkGray
+            tint = if (isError) Color.Red else Color.DarkGray
         )
 
         Spacer(modifier = Modifier.width(12.dp))
 
-        Text(
-            text = placeholder,
-            color = Color.Gray,
-            fontSize = 16.sp
+
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = TextStyle(
+                color = Color.Black,
+                fontSize = 16.sp
+            ),
+            decorationBox = { innerTextField ->
+                if (value.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        color = Color.Gray,
+                        fontSize = 16.sp
+                    )
+                }
+
+                innerTextField()
+            }
         )
     }
 }
-
+// Suppresslint usado para retirar warning do viewmodel dentro do composable
+@SuppressLint("ViewModelConstructorInComposable")
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun LoginScreenPreview() {
-    LoginScreen()
+    LoginScreen(loginViewModel = LoginViewModel())
 }
