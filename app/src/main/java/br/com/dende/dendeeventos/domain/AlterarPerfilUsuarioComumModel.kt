2cc926faec75d7ae1991bbe74aa65c4a8692ebb4
joalisson-p-maia <namibo654@gmail.com>
2026-05-21 19:42:40 -0300

    refactor: conforme solicitado, enviando data class AlterarPerfilUsuarioComum atualizado

--- app/src/main/java/br/com/dende/dendeeventos/domain/AlterarPerfilUsuarioComumModel.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/AlterarPerfilUsuarioComumModel.kt
@@ -1,13 +1,7 @@
-package main.java.br.com.dende.dendeeventos.domain
+package br.com.dende.dendeeventos.domain
+import br.com.dende.dendeeventos.domain.PerfilUsuario
 
-import java.time.LocalDateTime
-import main.java.br.com.dende.dendeeventos.domain.Genero
-
-data class AlterarPerfilUsuarioComumModel(
-    var nome: String,
-    var email: String,
-    var genero: Genero,
-    var caminhoFotoPerfil: String? = null,
-    var dataNascimento: LocalDateTime,
-    var ativo: Boolean = true
+data class AlterarPerfilUsuarioComumModel (
+    val perfilUsuario: PerfilUsuario,
+    var perfilAtivo: Boolean = true
 )
