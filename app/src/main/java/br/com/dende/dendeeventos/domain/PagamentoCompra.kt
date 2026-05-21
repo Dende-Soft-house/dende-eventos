9541fc23798c193fc2e952941a7039729aa83107
Levi Falcão | Hpnos <40741874+hpnos@users.noreply.github.com>
2026-05-21 19:19:46 -0300

    Update PagamentoCompra.kt

--- app/src/main/java/br/com/dende/dendeeventos/domain/PagamentoCompra.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/PagamentoCompra.kt
@@ -1,9 +1,5 @@
 import java.util.UUID
-
-
-enum class StatusPagamento {
-    PENDENTE, CONFIRMADO, RECUSADO, CANCELADO
-}
+import br.com.dende.dendeeventos.components.StatusPagamento
 
 data class Pagamento(
     val id: String = UUID.randomUUID().toString(),
@@ -12,7 +8,6 @@ data class Pagamento(
     val cartaoId: String? = null,
     val tipoPagamento: TipoPagamento,
     val quantidade: Int,
-    val precoUnitario: Double,
     val valorTotal: Double,
     var status: StatusPagamento = StatusPagamento.PENDENTE,
     val dataHora: LocalDateTime
