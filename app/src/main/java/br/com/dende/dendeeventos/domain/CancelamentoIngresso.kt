c84fc41049cd16cb902b9b629c6ffcc8adc24b23
Levi Falcão | Hpnos <40741874+hpnos@users.noreply.github.com>
2026-05-21 10:15:45 -0300

    Ajustando a modelagem conforme solicitado.

--- app/src/main/java/br/com/dende/dendeeventos/domain/CancelamentoIngresso.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/CancelamentoIngresso.kt
@@ -1,14 +1,13 @@
 package br.com.dende.dendeeventos.domain
 
+import java.math.BigDecimal
+
 data class CancelamentoIngresso(
-    val idPedido: String,
     val ingresso: Ingresso,
-    var statusCancelamento: StatusCancelamento.NAO_INICIADO,
-    val percentualTaxaCancelamento: Int, // de 0 a 100
+    var statusCancelamento: StatusCancelamento = StatusCancelamento.NAO_INICIADO,
     var motivoCancelamento: TipoMotivoCancelamento = TipoMotivoCancelamento.OUTRO,
     val observacaoCancelamento: String? = null,
     val valorOriginal: BigDecimal,
-    val taxaCancelamento: BigDecimal, // Ver Tipo (float talvez ou algo similar)
     val valorReembolso: BigDecimal,
     val metodoPagamento: String,
 )
