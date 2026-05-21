dabe0c279f5cedceccf155d0872aba2454535228
Levi Falcão | Hpnos <40741874+hpnos@users.noreply.github.com>
2026-05-21 19:38:02 -0300

    eventoId from Pagamento data class

--- app/src/main/java/br/com/dende/dendeeventos/domain/PagamentoCompra.kt
+++ app/src/main/java/br/com/dende/dendeeventos/domain/PagamentoCompra.kt
@@ -4,7 +4,6 @@ import br.com.dende.dendeeventos.components.StatusPagamento
 data class Pagamento(
     val id: String = UUID.randomUUID().toString(),
     val ingresso: Ingresso,
-    val eventoId: String,
     val tipoPagamento: TipoPagamento,
     val quantidade: Int,
     val valorTotal: BigDecimal,
